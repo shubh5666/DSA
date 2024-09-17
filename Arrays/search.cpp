cdad4a1406eb81b785f9a33c53518446d45186b0
shubham chauhan <154400060+shubh5666@users.noreply.github.com>
2024-09-17 11:02:54 +0530

    Add files via upload

---
+++ Arrays/search.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={3,5,8,4,2};
+    
+    for(int i=0;i<5;i++){
+        if(arr[i]==8){
+         cout<<i<<" ";
+            
+        }
+       
+    }
+ 
+}
